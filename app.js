4f5a087656df865bebc3f79f3f2ef375517930b4
gretten <any@anykey.com>
2021-02-26 15:11:54 +0300

    it works

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require('express');
-const mongoose = require('mongoose')
-
+const mongoose = require('mongoose');
+const { connect } = require('./routes/posts');
 const app = express();
+const bodyParser = require('body-parser')
+const cors = require('cors');
+require('dotenv/config')
+
+app.use(bodyParser.json());
+app.use(cors());
+// import routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
 
 app.get('/', (req, res) => {
-    res.send('we are at homie')
+    res.send('We are at home!')
 });
 
-app.get('/posts', (req, res) => {
-    res.send('we are at posts')
-});
-
-mongoose.connect('mongodb+srv://root:a6bx6vyt@rest.gapaf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
-    { useNewUrlParser: true, useUnifiedTopology: true },
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true },
     () => {
         console.log('connected to DB')
     })
